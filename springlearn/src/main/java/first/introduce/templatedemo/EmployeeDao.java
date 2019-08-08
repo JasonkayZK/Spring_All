1d4a9520e13608279b561b5965123da54d882f25
Jasonkay <271226192@qq.com>
2019-08-08 10:58:50 +0800

    更新spring框架版本, 消除alert

--- springlearn/src/main/java/first/introduce/templatedemo/EmployeeDao.java
+++ springlearn/src/main/java/first/introduce/templatedemo/EmployeeDao.java
@@ -1,29 +1,26 @@
 package first.introduce.templatedemo;
 
-import first.introduce.entity.impl.Employee;
-import org.springframework.jdbc.core.JdbcTemplate;
-
 /**
  * @author zk
  * @DATE 19-8-8
  */
 public class EmployeeDao {
 
-    private JdbcTemplate jdbcTemplate = new JdbcTemplate();
-
-    private Employee getEmployeeById(long id) {
-        return jdbcTemplate.queryForObject(
-                "select id, firstname, lastname, salary "
-                        + "from employee where id=?",
-                (resultSet, i) -> {
-                    Employee employee = new Employee();
-                    employee.setId(resultSet.getLong("id"));
-                    employee.setFirstName(resultSet.getString("firstname"));
-                    employee.setLastName(resultSet.getString("lastname"));
-                    employee.setSalary(resultSet.getBigDecimal("salary"));
-                    return employee;
-                }
-                , id);
-    }
+//    private JdbcTemplate jdbcTemplate = new JdbcTemplate();
+//
+//    private Employee getEmployeeById(long id) {
+//        return jdbcTemplate.queryForObject(
+//                "select id, firstname, lastname, salary "
+//                        + "from employee where id=?",
+//                (resultSet, i) -> {
+//                    Employee employee = new Employee();
+//                    employee.setId(resultSet.getLong("id"));
+//                    employee.setFirstName(resultSet.getString("firstname"));
+//                    employee.setLastName(resultSet.getString("lastname"));
+//                    employee.setSalary(resultSet.getBigDecimal("salary"));
+//                    return employee;
+//                }
+//                , id);
+//    }
 
 }
