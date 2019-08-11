5dd60517f2ed4cde682bf7a25c4e71c277da9b1b
Jasonkay <271226192@qq.com>
2019-08-11 22:33:37 +0800

    开启Spring Web Flow的学习

---
+++ springwebflow/src/main/java/club/jasonkayzk666/test/ConfigTest.java
@@ -0,0 +1,15 @@
+package club.jasonkayzk666.test;
+
+import org.junit.Test;
+
+/**
+ * @author zk
+ * @DATE 19-8-11
+ */
+public class ConfigTest {
+
+    @Test
+    public void checkConfig() {
+        System.out.println("OK!");
+    }
+}
